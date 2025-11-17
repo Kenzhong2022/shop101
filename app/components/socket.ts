4f8852c1be28620524091c69dd2f1b28263851be
kenZhongKai <997260348@qq.com>
2025-11-18 02:07:02 +0800

    测试ws

--- app/components/socket.ts
+++ app/components/socket.ts
@@ -3,7 +3,10 @@ import { io } from "socket.io-client";
 const socketUrl =
   import.meta.env.NUXT_PUBLIC_SOCKET_URL || "ws://localhost:3000";
 // 创建 Socket.IO 客户端实例
-export const socket = io(socketUrl, {
-  autoConnect: false, // 先不连
-  transports: ["websocket", "polling"], // 优先使用 WebSocket 传输
-});
+export const socket = io(
+  "wss://shop101-socket-server-production.up.railway.app",
+  {
+    autoConnect: false, // 先不连
+    transports: ["websocket", "polling"], // 优先使用 WebSocket 传输
+  }
+);

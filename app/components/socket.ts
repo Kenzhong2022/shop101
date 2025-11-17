59cddeade7a62c69d7d4bf670c6ca52dcf19fa78
kenZhongKai <997260348@qq.com>
2025-11-18 02:07:33 +0800

    测试ws

--- app/components/socket.ts
+++ app/components/socket.ts
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 // 从环境变量获取 Socket 服务器 URL，默认值为 localhost:3000
-const socketUrl =
-  import.meta.env.NUXT_PUBLIC_SOCKET_URL || "ws://localhost:3000";
+
 // 创建 Socket.IO 客户端实例
 export const socket = io(
   "wss://shop101-socket-server-production.up.railway.app",

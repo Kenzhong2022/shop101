e533902b02be697123299486863dcc1dd5195d5f
kenZhongKai <997260348@qq.com>
2025-11-18 17:12:48 +0800

    修改配置socket

--- app/components/socket.ts
+++ app/components/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 // 读取 Netlify 注入的变量，本地 fallback 到 localhost:8888
-export const socket = io("wss://firstly-pipier-jensen.ngrok-free.dev", {
+export const socket = io("https://firstly-pipier-jensen.ngrok-free.dev", {
   autoConnect: false,
+  transports: ["websocket", "polling"],
 });

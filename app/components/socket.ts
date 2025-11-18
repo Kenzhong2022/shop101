f75c13474a6f9338df0d201d72b29ba86a6a6dde
kenZhongKai <997260348@qq.com>
2025-11-18 17:03:22 +0800

    socket配置

--- app/components/socket.ts
+++ app/components/socket.ts
@@ -1,8 +1,5 @@
 import { io } from "socket.io-client";
 // 读取 Netlify 注入的变量，本地 fallback 到 localhost:8888
-export const socket = io(
-  "wss://shop101-socket-server-production.up.railway.app",
-  {
-    autoConnect: false,
-  }
-);
+export const socket = io("wss://firstly-pipier-jensen.ngrok-free.dev", {
+  autoConnect: false,
+});

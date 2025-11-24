0f54b354b3f9b6e3b67fd8e1d0e80437cbe3ad33
kenZhongKai <997260348@qq.com>
2025-11-25 02:03:27 +0800

    延迟映入all.js包

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,11 +75,11 @@ export default defineNuxtConfig({
           src: "https://cdn.jsdelivr.net/npm/colorthief@2.3.2/dist/color-thief.umd.js",
           defer: true, // 异步加载，不阻塞渲染
         },
-        {
-          // 引入 Cloudinary 上传小部件
-          src: "https://upload-widget.cloudinary.com/global/all.js",
-          defer: true, // 异步加载，不阻塞渲染
-        },
+        // {
+        //   // 引入 Cloudinary 上传小部件
+        //   src: "https://upload-widget.cloudinary.com/global/all.js",
+        //   defer: true, // 异步加载，不阻塞渲染
+        // },
       ],
     },
   },

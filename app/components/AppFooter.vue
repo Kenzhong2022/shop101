aa8a6a3904dc67f7b2e9ddf775286bb1b5c228d8
kenZhongKai <997260348@qq.com>
2025-11-24 16:49:38 +0800

    调整底部布局

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -3,9 +3,7 @@
   此文件定义了应用程序的页脚组件。
 -->
 <template>
-  <footer
-    class="text-center text-gray-600 position-absolute bottom-0 left-0 right-0 bg-primary py-4"
-  >
+  <footer class="text-center text-gray-600 bg-primary py-4">
     <p class="text-white">&copy; 2024 我的 Nuxt 网站</p>
   </footer>
 </template>
